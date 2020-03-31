995f446953fe195460fca512bcaec8b96200305c
Unknown <lars.kohfahl@physik.tu-darmstadt.de>
2020-03-31 15:22:54 +0200

    For fast profile switching on trigger: change triggerPin
    plus documentation

--- AD9910_triggered_profile_switch_fast/AD9910_triggered_profile_switch_fast.ino
+++ AD9910_triggered_profile_switch_fast/AD9910_triggered_profile_switch_fast.ino
@@ -10,7 +10,7 @@
 #define PS2PIN 7                        // DDS PROFILE[2] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
 #define IO_UPDATEPIN  9                 // DDS I/O_UPDATE pin. Digital input. A high on this pin transfers the contents of the buffers to the internal registers.
 #define RESETPIN 10                     // DDS MASTER_RESET pin. Digital input. Clears all memory elements and sets registers to default values.
-#define TRIGGERIN 28   //Pin28=D3       // Pin to trigger the Arduino
+#define TRIGGERIN 35   //Pin28=D3, 35=C3// Pin to trigger the Arduino
 #define TRIGGEROUT 13  //Pin13=B27      // Pin to trigger events with the Arduino
 
 int divider=25;                         // System clock is ref clk * divider
@@ -38,13 +38,13 @@ void setup() {
   delay(10);
   
   DDS.initialize(ref_clk,divider);
-  DDS.setFreqAmp(10000000,1.0,0);
+  DDS.setFreqAmp(1000000,1.0,0);
   DDS.setFreqAmp(1000000,0.9,1);
-  DDS.setFreqAmp(10000000,0.8,2);
+  DDS.setFreqAmp(1000000,0.8,2);
   DDS.setFreqAmp(1000000,0.7,3);
-  DDS.setFreqAmp(10000000,0.6,4);
+  DDS.setFreqAmp(1000000,0.6,4);
   DDS.setFreqAmp(1000000,0.5,5);
-  DDS.setFreqAmp(10000000,0.4,6);
+  DDS.setFreqAmp(1000000,0.4,6);
   DDS.setFreqAmp(1000000,0,7);
   DDS.setProfile(0);
 
@@ -57,13 +57,22 @@ void setup() {
 }
 
 void loop() {
-  PIOD->PIO_CODR = PIO_CODR_P3;  // Be sure to start with a low level for output pin (
+  PIOC->PIO_CODR = PIO_CODR_P3;  // Be sure to start with a low level for output pin
+  //Assumed working principle: PIOD is a pointer to Port D (all D-Pins).
+  //PIO_CODR is a 32bit register which is set 1 for Pin3 meaning it will set Pin3 low as it sets all high-entries to low
+  //Due to the dereference operator -> Port D is set as PIO_CODR -> Pin3 is set low. 
   while (true) {
     // wait until pin 28 is high
-    while ((PIOD->PIO_PDSR & PIO_PDSR_P3) == 0);
+    while ((PIOC->PIO_PDSR & PIO_PDSR_P3) == 0);
+    // PIO_PDSR_P3 is a data status register for Pin3 (bit 3 is high)
+    // PIOD->PIO_PDSR is the current value of the data status register of Port D
+    // PDSR is high if Input is high
+    // If they are both 1 the AND-operator & makes the result 1 and we exit the while loop
 
     // Toggle pin 13
     PIOB->PIO_ODSR ^= PIO_ODSR_P27;  // low to high by using xor operation between ...?
+    // PIO_ODSR_P27 is high at 27th bit.
+    // Does new value of PIO_ODSR of Port B (PIOB->PIO_ODSR) is the xor-result of old PIOB->PIO_ODSR and PIO_ODSR_P27
     
     DDS.setProfileFast(state);
     state++;
