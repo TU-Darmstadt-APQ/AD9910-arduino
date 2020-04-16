520e59d9567ad756724aa1502d2b4f3db9bd91f4
Unknown <lars.kohfahl@physik.tu-darmstadt.de>
2020-04-16 12:50:47 +0200

    Update Pin connections

--- preliminary_versions_and_helpers/AD9910_triggered_profile_switch_fast/AD9910_triggered_profile_switch_fast.ino
+++ preliminary_versions_and_helpers/AD9910_triggered_profile_switch_fast/AD9910_triggered_profile_switch_fast.ino
@@ -5,11 +5,11 @@
 //Define pin mappings:
 #define CSPIN 14                        // DDS chip select pin. Digital input (active low). Bringing this pin low enables detection of serial clock edges.
 #define OSKPIN 15                       // DDS Output Shift Keying. Digital input.
-#define PS0PIN 5                        // DDS PROFILE[0] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
-#define PS1PIN 6                        // DDS PROFILE[1] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
-#define PS2PIN 7                        // DDS PROFILE[2] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
-#define IO_UPDATEPIN  9                 // DDS I/O_UPDATE pin. Digital input. A high on this pin transfers the contents of the buffers to the internal registers.
-#define RESETPIN 10                     // DDS MASTER_RESET pin. Digital input. Clears all memory elements and sets registers to default values.
+#define PS0PIN 25                        // DDS PROFILE[0] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
+#define PS1PIN 26                        // DDS PROFILE[1] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
+#define PS2PIN 27                        // DDS PROFILE[2] pin. Profile Select Pins. Digital input. Use these pins to select one of eight profiles for the DDS.
+#define IO_UPDATEPIN  11                // DDS I/O_UPDATE pin. Digital input. A high on this pin transfers the contents of the buffers to the internal registers.
+#define RESETPIN 12                     // DDS MASTER_RESET pin. Digital input. Clears all memory elements and sets registers to default values.
 #define TRIGGERIN 35   //Pin28=D3, 35=C3// Pin to trigger the Arduino
 #define TRIGGEROUT 13  //Pin13=B27      // Pin to trigger events with the Arduino
 
@@ -38,6 +38,7 @@ void setup() {
   delay(10);
   
   DDS.initialize(ref_clk,divider);
+/*
   DDS.setFreqAmp(1000000,1.0,0);
   DDS.setFreqAmp(1000000,0.9,1);
   DDS.setFreqAmp(1000000,0.8,2);
@@ -47,7 +48,7 @@ void setup() {
   DDS.setFreqAmp(1000000,0.4,6);
   DDS.setFreqAmp(1000000,0,7);
   DDS.setProfile(0);
-
+*/
   delay (10);
   pinMode(TRIGGERIN, INPUT);
   pinMode(TRIGGEROUT, OUTPUT);
